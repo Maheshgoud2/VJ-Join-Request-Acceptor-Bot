882a03e20624b8d31cca2f9044159ba9c022ac29
Maheshgoud2 <107980963+Maheshgoud2@users.noreply.github.com>
2025-05-14 06:18:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002663107514"))
 ADMINS = int(environ.get("ADMINS", "-1008098368518"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = environ.get("DB_URI", "mongodb+srv://boyboy:boyboy@cluster0.ar8xyfh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
 
 # If this is True Then Bot Accept New Join Request 
